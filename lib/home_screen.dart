1220bcfe1547eed149f058c333661509c7210d6f
Salmanul Faris M <mzfarisi@gmail.com>
2023-05-30 00:22:30 +0530

    adding battery percentage package

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_package_tester/packages/google_fonts.dart';
-import 'package:google_fonts/google_fonts.dart';
+
+import 'models/list_package.dart';
 
 class HomeScreen extends StatelessWidget {
    HomeScreen({super.key});
@@ -15,29 +15,7 @@ class HomeScreen extends StatelessWidget {
       body: ListView.builder(
         itemCount: 10,
         itemBuilder: (context, index) {
-          return Card(
-            elevation: 30,
-            shadowColor: Colors.grey,
-            color: Colors.blue,
-            margin: const EdgeInsets.all(10),
-            child: SizedBox(
-              width: 250,
-              height: 100,
-              child: ListTile(
-                leading: Text(style: GoogleFonts.lato(), (index+1).toString()),
-                trailing: IconButton(
-                    icon:  Icon(Icons.person_search,size: 45),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => GoogleFontsSamples()),
-                      );
-                    }),
-                title: const Text("Google Fonts"),
-                subtitle: const Text('A Flutter package to use fonts from fonts.google.com.'),
-              ),
-            ),
-          );
+          return testfuntioncard(name1,name1sub,route1);
         },
       ),
     );
